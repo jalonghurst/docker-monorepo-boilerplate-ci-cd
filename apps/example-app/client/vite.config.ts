9b2cad9b4fb480dd48b8f6138e001e89040f88b4
unknown <jacqui.longhurst@btinternet.com>
2025-02-06 19:18:33 +0100

    fixed compatibility issue of mounting local node_modules on container: ignored and installed inside container for correct OS instead

--- apps/example-app/client/vite.config.ts
+++ apps/example-app/client/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 2000,
+    host: true,
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
 })
